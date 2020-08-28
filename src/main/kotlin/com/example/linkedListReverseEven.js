c57f35a2e4af4a25eb5a260e28a12e44a2ea8cf6
Prashanth Batchu <prashanth@batchu.net>
2020-08-27 21:15:51 -0400

    Final solution for reversing sub list in a linked list

--- src/main/kotlin/com/example/linkedListReverseEven.js
+++ src/main/kotlin/com/example/linkedListReverseEven.js
@@ -1,4 +1,27 @@
-// Add any extra import statements you may need here
+/*
+https://www.facebookrecruiting.com/portal/coding_practice_question/?problem_id=623634548182866
+Reverse Operations
+You are given a singly-linked list that contains N integers. A subpart of the list is a contiguous set of even elements, bordered either by the end of the list or an odd element. For example, if the list is [1, 2, 8, 9, 12, 16], the subparts of the list are [2, 8] and [12, 16].
+Then, for each subpart, the order of the elements is reversed. In the example, this would result in the new list, [1, 8, 2, 9, 16, 12].
+The goal of this question is: given a resulting list, determine the original order of the elements.
+Implementation detail:
+You must use the following definition for elements in the linked list:
+class Node {
+    int data;
+    Node next;
+}
+Signature
+Node reverse(Node head)
+Constraints
+1 <= N <= 1000, where N is the size of the list
+1 <= Li <= 10^9, where Li is the ith element of the list
+Example
+Input:
+N = 6
+list = [1, 2, 8, 9, 12, 16]
+Output:
+[1, 8, 2, 9, 16, 12]
+*/
 /*
   sol = head
   current = sol
@@ -18,55 +41,55 @@ class Node {
   
   /*
   say input is 1-3-6-8-2-7
-  i=3
-  j=3
-  now, we need to reverse 6 and 8 and return newI:7, newJ:7, reversedSubList:2-8-6
+  i=6
+  now, we need to reverse 6 and 8 and return newI:2, head:8, tail:6 (after reversing 6-8)
   */
   function reverseSubList (i){
-      //i=3
-    i=i.next // i = 6
-    let head = new Node(i.data) //head=6;i=6
-    let tail = head //head=6, tail = 6
+    let head = new Node(i.data) 
+    let tail = head 
     i=i.next
-    while(i!=null && i.data%2==0){    
-      const temp = head //temp = 6
-      head = new Node(i.data) //head=8
-      head.next=temp //head -> 8-6
-      i=i.next //i=2
+    while(i!=null && i.data%2==0){ 
+    //Since we are reversing the sub list, push out the current head (swap it with the next node)   
+      const temp = head 
+      head = new Node(i.data) 
+      head.next=temp 
+      i=i.next
     }
-   
+   //i will be the next od element
     return {newI:i,head:head, tail}
   }
   
   
   function reverse(head) {
-    
-    const sol = new Node(head.data)
-    let j = sol
+    // We are going to create our solution linked list with a dummy head because the first
+    // element of the input (head) could be the beginning of a even sub list. We will return sol.next
+    // to exclude the dummy head from the returned solution
+   const sol = new Node(-1)
+   let j = sol
    let i =head;
     while(i!=null){
-      if(i.next.data%2!=0)
+      if(i.data%2!=0)
         {
-          j.next= new Node(i.next.data)
+          j.next= new Node(i.data)
           i=i.next
+          j=j.next
         }
       else{
+        //Generate a reversed sub list. 
         const resp = reverseSubList(i)
-        //Since we just reversed the sub list and iterated through it, i moves to the end of the sub list
+        //Move the it to the new pointer (we just navigated through the sub list)
         i=resp.newI
         //Take the reverse sub list's head and attached it to the solution's current pointer i.e j
         j.next=resp.head
         //move j to point to the tail of the reversed sub list
         j=resp.tail
-        if(i!=null){
-            j.next = new Node(i.data)
-            j=j.next
-
-        }
-
+        //Don't forget to add the node that immediately follows the even sub list back to the solution.
+        //Check for null as it could be the last element in the sub list!
+        if(i!=null)
+        j.next=new Node(i.data)
       }
     }
-    return sol
+    return sol.next
     
   }
   
